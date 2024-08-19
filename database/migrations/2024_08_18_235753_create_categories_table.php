21a0b5f5c041426b9d04238f7fcb1895b2ff5f74
Hugobsan <hugocorone10@gmail.com>
2024-08-18 21:13:30 -0300

    add: Adicionando migration de categorias e configuração de paginação com bootstrap

--- database/migrations/2024_08_18_235753_create_categories_table.php
+++ database/migrations/2024_08_18_235753_create_categories_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->index();
+            $table->string('code')->unique();
+            $table->string('icon')->nullable();
+            $table->string('color')->nullable();
+            $table->text('description')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
