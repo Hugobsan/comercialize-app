77a63f3499b3c1e8f679c16158d69991c0c011a0
Hugobsan <hugocorone10@gmail.com>
2024-08-20 13:17:27 -0300

    fix: melhorando design de exibição

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -9,21 +9,20 @@
             <div class="col-12 col-md-4">
                 <img src="{{ $product->photo }}" class="img-fluid" alt="{{ $product->name }}">
             </div>
-            <div class="col-12 col-md-8">
-                <h1>{{ $product->name }}</h1>
-                <div style="color: {{ $product->category->color ?? '#33f' }}">
-                    <i class="{{ $product->category->icon }}"></i>
-                    {{ $product->category->name }}
-                </div>
-                <p>R$ {{ $product->price }}</p>
-                <p class="{{ $product->quantity <= 5 ? 'text-danger' : '' }}">Estoque: {{ $product->quantity }}</p>
-                <p>{{ $product->description }}</p>
-                <div class="d-flex flex-row justify-content-between">
-                    <div>
-                        <a href="{{ route('sales.add-to-cart', $product->id) }}" class="btn btn-success" data-bs-toggle="tooltip" title="Adicionar ao carrinho de compras">
-                            <i class="fas fa-cart-plus"></i>
-                        </a>
+            <div class="col-12 col-md-5 d-flex flex-column justify-content-between">
+                <div>
+                    <h1>{{ $product->name }}</h1>
+
+                    <div style="color: {{ $product->category->color ?? '#33f' }}">
+                        <i class="{{ $product->category->icon }}"></i>
+                        {{ $product->category->name }}
                     </div>
+                    <p>R$ {{ $product->price }}</p>
+                    <p class="{{ $product->quantity <= 5 ? 'text-danger' : '' }}">Estoque: {{ $product->quantity }}</p>
+                </div>
+            </div>
+            <div class="col-md-3 d-flex flex-column justify-content-between">
+                <div class="d-flex flex-row justify-content-end">
                     <div class="dropdown">
                         <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                             <i class="fas fa-bars"></i>
@@ -38,17 +37,21 @@
                                     <hr class="dropdown-divider">
                                 </li>
                                 <li class="btn-danger
-                                ">
+                            ">
                                     <form action="{{ route('products.destroy', $product->id) }}" method="post" style="display: inline;">
                                         @csrf
                                         @method('DELETE')
                                         <button type="submit" class="dropdown-item">Excluir</button>
                                     </form>
-
                                 </li>
                             @endcan
                         </ul>
                     </div>
+                </div>
+                <div class="d-flex flex-row justify-content-end">
+                    <a href="{{ route('sales.add-to-cart', $product->id) }}" class="btn btn-success" data-bs-toggle="tooltip" title="Adicionar ao carrinho de compras">
+                        <i class="fas fa-cart-plus"></i> Adicionar ao carrinho
+                    </a>
                 </div>
             </div>
         </div>
