d06f00468fbf825534ab289651ca7640ce4d06e1
Hugobsan <hugocorone10@gmail.com>
2024-08-22 23:59:31 -0300

    fix: melhorando estilo de exibição dos valores

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -11,36 +11,38 @@
 @endpush
 
 @section('content')
-    <div class="container">
+    <div class="container my-3">
         <form action="{{ route('sales.store') }}" method="POST">
             @csrf
-            <div class="form-group">
-                <label for="seller">Vendedor</label>
-                <select class="form-control" id="seller" name="seller" required>
-                    <option value="">Selecione um vendedor</option>
-                    @foreach ($sellers as $seller)
-                        <option value="{{ $seller->id }}">{{ $seller->name }}</option>
-                    @endforeach
-                </select>
-                @error('seller')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
+            <div class="row">
+                <div class="form-group col-12 col-md-6">
+                    <label for="seller">Vendedor</label>
+                    <select class="form-control" id="seller" name="seller" required>
+                        <option value="">Selecione um vendedor</option>
+                        @foreach ($sellers as $seller)
+                            <option value="{{ $seller->id }}">{{ $seller->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('seller')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
+
+                <div class="form-group col-12 col-md-6">
+                    <label for="customer">Cliente</label>
+                    <select class="form-control" id="customer" name="customer" required>
+                        <option value="">Selecione um cliente</option>
+                        @foreach ($customers as $customer)
+                            <option value="{{ $customer->id }}">{{ $customer->name }}</option>
+                        @endforeach
+                    </select>
+                    @error('customer')
+                        <span class="text-danger">{{ $message }}</span>
+                    @enderror
+                </div>
             </div>
 
-            <div class="form-group">
-                <label for="customer">Cliente</label>
-                <select class="form-control" id="customer" name="customer" required>
-                    <option value="">Selecione um cliente</option>
-                    @foreach ($customers as $customer)
-                        <option value="{{ $customer->id }}">{{ $customer->name }}</option>
-                    @endforeach
-                </select>
-                @error('customer')
-                    <span class="text-danger">{{ $message }}</span>
-                @enderror
-            </div>
-
-            <div class="table-responsive my-2 p-3 bg-white rounded shadow">
+            <div class="table-responsive my-3 p-3 bg-white rounded shadow">
                 <table class="table table-striped table-hover">
                     <thead>
                         <tr>
@@ -60,9 +62,9 @@
                     </tbody>
                 </table>
 
-                <div class="text-right">
-                    <p>Total de produtos: {{ count($cart_products) }}</p>
-                    <p>Soma total: R$ <span>{{ number_format($total, 2, ',', '.') }}</span></p>
+                <div class="text-right py-3">
+                    <p>Quant. de Produtos: {{ count($cart_products) }}</p>
+                    <p class="h3">Total: R$ <span>{{ number_format($total, 2, ',', '.') }}</span></p>
                 </div>
 
                 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addProductModal">
